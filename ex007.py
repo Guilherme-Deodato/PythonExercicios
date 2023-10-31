64292fcbcde2246e8643e1d1eda872a9358c8e1d
Guilherme Deodato <78092037+Guilherme-Deodato@users.noreply.github.com>
2023-10-30 22:01:22 -0300

    Exercícios em Python

---
+++ ex007.py
@@ -0,0 +1,7 @@
+# Deselvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
+
+nota01 = float(input('Insira a primeira nota: '))
+nota02 = float(input('Insira a segunda nota: '))
+media = (nota01 + nota02) / 2
+
+print(f'O aluno obteve media {media}')
